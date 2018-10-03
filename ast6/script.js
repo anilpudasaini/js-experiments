34e143a7b91c7cf78cfdfa524fc2127cda35d67f
anilPudasaini <pudasaini.anil17@gmail.com>
2018-10-03 13:12:04 +0545

    car game

--- ast6/script.js
+++ ast6/script.js
@@ -11,15 +11,25 @@ var carHeight = 100;
 var carX = (canvas.width - carWidth)/2;
 var carY = canvas.height - carHeight;
 var carSpeed = 10;
-var enemySpawnlocation = [carX,50,355];
+var enemySpawnlocation = [carX,50,300];
 var enemyCarX = enemySpawnlocation[Math.floor(Math.random() * enemySpawnlocation.length)];
 var enemyCarY = 0;
+
 var collideSound;
 var flag = false; 
 var ref;
 
-//for controlling loop
-// console.log(enemyCarX);
+
+
+
+var img = new Image();
+var playerCar = new Image();
+var enemyCar = new Image();
+
+    
+img.src = 'images/backg.png';
+playerCar.src = 'images/main.png';
+enemyCar.src = 'images/car1.png';
 
 
 
@@ -27,16 +37,34 @@ document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 
-function game()
-{
-getCanvas();
-// moveCanvas();
-drawCar();
-drawEnemyCar();
-updateCarLocation();
-moveEnemyCar();
-detectCollision();	
+function bgDraw(x, y) {
+    this.x = x;
+    this.y = y;
+    this.dy = 5;
+
+    this.drawTrack = function() {
+        cxt.drawImage(img, this.x, this.y);
+    }
+
+    
+
+    this.updatebgDraw = function() {
+        this.drawTrack();
+        if (this.y >= 575) {
+            this.y = -575;
+        }
+        this.y += this.dy;
+    }
 }
+
+
+var initialBg = new bgDraw(0, 0);
+var finalBg = new bgDraw(0, -575);
+var enemy1 = new drawEnemyCar();
+
+
+
+
 
 function sound(src) {
     this.sound = document.createElement("audio");
@@ -56,59 +84,79 @@ function sound(src) {
 
 
 
-function getCanvas()
-{
-	var img = new Image();
-	img.src = 'images/bg.jpg';
-	var pattern = cxt.createPattern(img, 'repeat');
-	cxt.fillStyle = pattern;
-	cxt.fillRect(0, 0, canvas.width,canvas.height);
-
-}
-
-
 
 
 function drawCar() {
-    cxt.beginPath();
-    cxt.rect(carX, carY, carWidth, carHeight);
-    // cxt.fillStyle = "red";
-    // cxt.fill();
-    cxt.closePath();
-    // console.log('from draw car');
-    var imge = new Image();
-	imge.src = 'images/main.png';
-	cxt.drawImage(imge,carX,carY,carWidth,carHeight);	
+
+    cxt.drawImage(playerCar,carX,carY,carWidth,carHeight);   
 }
 
-function drawEnemyCar(){
+function drawEnemyCar() {
+    this.x = enemyCarX;
+    console.log(this.x);
+    this.y = 0;
+    this.dy = 10;
 
-	cxt.beginPath();
-    cxt.rect(enemyCarX,enemyCarY, carWidth, carHeight);
-    // cxt.fillStyle = "red";
-    // cxt.fill();
-    cxt.closePath();
-    // console.log('from draw car');
-    var imges = new Image();
-	imges.src = 'images/car_5.png';
-	cxt.drawImage(imges,enemyCarX,enemyCarY,carWidth,carHeight);
+
+    this.drawEnemy = function() {
+        cxt.drawImage(enemyCar, this.x, this.y,carWidth,carHeight);
+    }
+
+    this.enemyUpdate = function() {
+        this.drawEnemy();
+        this.y += this.dy;
+        if (this.y >= canvas.height) {
+            this.x = enemySpawnlocation[Math.floor(Math.random() * enemySpawnlocation.length)];
+            // this.y = enemyCarX;
+            console.log('hell');
+            console.log()
+            this.y = -carHeight - 10;
+        }
+    }
 }
 
+function updateCarLocation()
+{
+    if (rightKeyPressed && carWidth+carX < canvas.width ) {
+        carX += carSpeed;   
+    }
+    if (leftKeyPressed && carX > 0) {
+        carX -= carSpeed;
+    }
+}
 
+function detectCollision()
+{
+
+
+	    if((carX + carWidth) > enemy1.x && carX < (enemy1.x + carWidth) && (enemy1.y + carHeight) > carY && (carY + carHeight) > enemy1.y)
+	    {
+	        flag = true;
+
+	        gameOver();
+	    }
+}
 
 function keyDownHandler(e) {
 
-	if ( e.keyCode === 13)
-	{
-		loop();
-		console.log('doneeeeee');
-	}
+    if ( e.keyCode === 83)
+    {
+        clearInterval(ref);
+        console.log('doneeeeee');
+    }
+
+    if ( e.keyCode === 82)
+    {
+    	clearInterval(ref);
+       	loop();
+        console.log('doneeeeee');
+    }
 
     if (e.keyCode === 39) {
         rightKeyPressed = true;
 
 
-
+        
     } else if (e.keyCode === 37) {
         leftKeyPressed = true;
     }
@@ -125,53 +173,35 @@ function keyUpHandler(e) {
 }
 
 
-function updateCarLocation()
-{
-    if (rightKeyPressed && carWidth+carX < canvas.width ) {
-        carX += carSpeed;	
-    }
-    if (leftKeyPressed && carX > 0) {
-        carX -= carSpeed;
-    }
-}
 
-function moveEnemyCar()
-{
-	if (true) {
-        enemyCarY += 2;	
-    }
-    
-}
-
-function detectCollision()
-{
-	
-	if((carX + carWidth) > enemyCarX && carX < (enemyCarX + carWidth) && (enemyCarY + carHeight) > carY && (carY + carHeight) > enemyCarY)
-	{
-		flag = true;
-
-		gameOver();
-	}
-}
 
 
 function gameOver()
 {
-collideSound = new sound("sound/1.wav");
-collideSound.play()
-cxt.fillStyle = 'black';
-cxt.fillRect(0,0,canvas.width,canvas.height);
-cxt.font="30px Verdana";
-var gradient=cxt.createLinearGradient(0,0,canvas.width,0);
-gradient.addColorStop("0.3","green");
-gradient.addColorStop("0.5","blue");
-gradient.addColorStop("1.0","red");
+	clearInterval(ref);
+	cxt.clearRect(0,0,canvas.width,canvas.height);
+	cxt.beginPath();
+	console.log(enemy1);
+	// clearInterval(ref);
+	console.log(enemy1);
+	// initialBg.drawTrack();
+    collideSound = new sound("sound/1.wav");
+    collideSound.play()
+    cxt.fillStyle = 'black';
+    cxt.fillRect(enemy1.x,enemy1.y,carWidth,carHeight);
+    cxt.fillRect(0,0,canvas.width,canvas.height);
+    cxt.font="20px Verdana";
+    var gradient=cxt.createLinearGradient(0,0,canvas.width,0);
+    gradient.addColorStop("0.3","green");
+    gradient.addColorStop("0.5","blue");
+    gradient.addColorStop("1.0","red");
+
 // Fill with gradient
-cxt.fillStyle=gradient;
-cxt.fillText("Game over !!!!",50,100,200);
-cxt.fillText("You Survived for " + survivalTime() + 'seconds' ,40,200);
-console.log(survivalTime);
-clearInterval(ref);
+	cxt.fillStyle=gradient;
+	cxt.fillText("Game over !!!!",50,100,200);
+	cxt.fillText("You Survived for " + survivalTime() + 'seconds' ,40,200);
+	// console.log();
+	cxt.closePath();
 
 
 
@@ -179,31 +209,45 @@ clearInterval(ref);
 
 function survivalTime()
 {
-	var survivalTime = Date.now() - timer;
-	return Math.floor(survivalTime/1000);
+    var survivalTime = Date.now() - timer;
+    return Math.floor(survivalTime/1000);
 }
 
+
+
+function game()
+{
+initialBg.updatebgDraw()
+finalBg.updatebgDraw();
+
+enemy1.enemyUpdate();
+drawCar();
+
+detectCollision();
+updateCarLocation();
+
+}
+
+
+
 function loop()
-{
+	{
 
-ref = setInterval(function()
-{
+		    ref = setInterval(function()
+		    {
 
-	cxt.clearRect(0,0,canvas.width,canvas.height);
-	game();
- 	// requestAnimationFrame(loop);
-	console.log('requestId');	
-	
-}, 1000/60);
-
-}		
+		        cxt.clearRect(0,0,canvas.width,canvas.height);
+		        game();
+		    
+		    
+		}, 1000/60);
+	}       
 
 
 window.onload = function()
 {
-	loop();
+    loop();
 };
-
 
 
 
